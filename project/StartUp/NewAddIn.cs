792915cbd7e393dd3c48418a6284dfc02a2f1c09
Doug Klassen <d.p.klassen@gmail.com>
2016-04-20 10:05:27 -0700

    Updated file locations and vs project names for 2016

--- project/StartUp/NewAddIn.cs
+++ project/StartUp/NewAddIn.cs
@@ -8,62 +8,63 @@ using Autodesk.Revit.UI;
 
 namespace DougKlassen.Revit.NewAddIn.StartUp
 {
-    public static class FileLocations
-    {
-        //AddInDirectory is initialized at runtime
-        public static String AddInDirectory;
-        public static String AssemblyName;
-        public static readonly String imperialTemplateDirectory = @"C:\ProgramData\Autodesk\RVT 2014\Family Templates\English_I\";
-        public static readonly String ResourceNameSpace = @"DougKlassen.Revit.NewAddIn.Resources";
-    }
+	public static class FileLocations
+	{
+		//AddInDirectory is initialized at runtime
+		public static String AddInDirectory;
+		//AssemblyName is initialized at runtime
+		public static String AssemblyName;
+		public static readonly String imperialTemplateDirectory = @"X:\CAD\_REVIT 2016\Family Templates\English_I\";
+		public static readonly String ResourceNameSpace = @"DougKlassen.Revit.NewAddIn.Resources";
+	}
 
-    public class StartUpApp : IExternalApplication
-    {
-        Result IExternalApplication.OnStartup(UIControlledApplication application)
-        {
-            //initialize AssemblyName using reflection
-            FileLocations.AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
-            //initialize AddInDirectory. The addin should be stored in a directory named after the assembly
-            FileLocations.AddInDirectory = application.ControlledApplication.AllUsersAddinsLocation + "\\" + FileLocations.AssemblyName + "\\";
+	public class StartUpApp : IExternalApplication
+	{
+		Result IExternalApplication.OnStartup(UIControlledApplication application)
+		{
+			//initialize AssemblyName using reflection
+			FileLocations.AssemblyName = Assembly.GetExecutingAssembly().GetName().Name;
+			//initialize AddInDirectory. The addin should be stored in a directory named after the assembly
+			FileLocations.AddInDirectory = application.ControlledApplication.AllUsersAddinsLocation + "\\" + FileLocations.AssemblyName + "\\";
 
-            //load image resources
-            BitmapImage largeIcon = GetEmbeddedImageResource("iconLarge.png");
-            BitmapImage smallIcon = GetEmbeddedImageResource("iconSmall.png");
-            
-            PushButtonData newCommandPushButtonData = new PushButtonData(
-                "NewCommandButton", //name of the button
-                "NewCommand", //text on the button
-                FileLocations.AddInDirectory + FileLocations.AssemblyName + ".dll",
-                "DougKlassen.Revit.NewAddIn.Commands.NewCommand");
-            newCommandPushButtonData.LargeImage = largeIcon;
+			//load image resources
+			BitmapImage largeIcon = GetEmbeddedImageResource("iconLarge.png");
+			BitmapImage smallIcon = GetEmbeddedImageResource("iconSmall.png");
 
-            RibbonPanel newAddInRibbonPanel = application.CreateRibbonPanel("NewAddIn");
-            newAddInRibbonPanel.AddItem(newCommandPushButtonData);
+			PushButtonData newCommandPushButtonData = new PushButtonData(
+				 "NewCommandButton", //name of the button
+				 "NewCommand", //text on the button
+				 FileLocations.AddInDirectory + FileLocations.AssemblyName + ".dll",
+				 "DougKlassen.Revit.NewAddIn.Commands.NewCommand");
+			newCommandPushButtonData.LargeImage = largeIcon;
 
-            return Result.Succeeded;
-        }
+			RibbonPanel newAddInRibbonPanel = application.CreateRibbonPanel("NewAddIn");
+			newAddInRibbonPanel.AddItem(newCommandPushButtonData);
 
-        Result IExternalApplication.OnShutdown(UIControlledApplication application)
-        {
-            return Result.Succeeded;
-        }
+			return Result.Succeeded;
+		}
 
-        /// <summary>
-        /// Utility method to retrieve an embedded image resource from the assembly
-        /// </summary>
-        /// <param name="resourceName">The name of the image, corresponding to the filename of the embedded resouce added to the solution</param>
-        /// <returns>The loaded image represented as a BitmapImage</returns>
-        BitmapImage GetEmbeddedImageResource(String resourceName)
-        {
-            Assembly asm = Assembly.GetExecutingAssembly();
-            Stream str = asm.GetManifestResourceStream(FileLocations.ResourceNameSpace + "." + resourceName);
+		Result IExternalApplication.OnShutdown(UIControlledApplication application)
+		{
+			return Result.Succeeded;
+		}
 
-            BitmapImage bmp = new BitmapImage();
-            bmp.BeginInit();
-            bmp.StreamSource = str;
-            bmp.EndInit();
+		/// <summary>
+		/// Utility method to retrieve an embedded image resource from the assembly
+		/// </summary>
+		/// <param name="resourceName">The name of the image, corresponding to the filename of the embedded resouce added to the solution</param>
+		/// <returns>The loaded image represented as a BitmapImage</returns>
+		BitmapImage GetEmbeddedImageResource(String resourceName)
+		{
+			Assembly asm = Assembly.GetExecutingAssembly();
+			Stream str = asm.GetManifestResourceStream(FileLocations.ResourceNameSpace + "." + resourceName);
 
-            return bmp;
-        }
-    }
+			BitmapImage bmp = new BitmapImage();
+			bmp.BeginInit();
+			bmp.StreamSource = str;
+			bmp.EndInit();
+
+			return bmp;
+		}
+	}
 }
